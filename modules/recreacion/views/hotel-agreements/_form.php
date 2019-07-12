21a1af46b241507b91cc09089287cb9b75984030
cris-hub <>
2019-07-12 14:10:26 -0500

    last version manager content of hotels(plans/agreements) by production

--- modules/recreacion/views/hotel-agreements/_form.php
+++ modules/recreacion/views/hotel-agreements/_form.php
@@ -24,7 +24,7 @@ use yii\widgets\DetailView;
         
         <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
         
-        <!-- <?= $form->field($model, 'max_guests')->textInput(['maxlength' => true]) ?> -->
+         <?= $form->field($model, 'max_guests')->textInput(['maxlength' => true]) ?> 
 
         <?=
         $form->field($model, 'description')->widget(CKEditor::className(), [
