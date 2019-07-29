5ad177e2642ffdbecaca7a6c195660e5f037db85
cris-hub <crisss198@gmail.com>
2019-07-29 16:14:40 -0500

    virtual directory

--- ls.sh
+++ ls.sh
@@ -1,7 +1,7 @@
 az webapp create-remote-connection --subscription f08123f7-75c4-487d-875c-7d7a1150c6bf --resource-group Digital-Produccion-RyT-Hoteles -n cols-as-digprod-ryt-hoteles-content-front --verbose &
 az extension add -n cols-as-digprod-ryt-hoteles-content-front
 az extension update -n  cols-as-digprod-ryt-hoteles-content-front webapp
-az webapp remote-connection create -g Digital-Produccion-RyT-Hoteles -n cols-as-digprod-ryt-hoteles-content-front -p 9000  --verbose &
+az webapp remote-connection create -g Digital-Produccion-RyT-Hoteles -n cols-as-digprod-ryt-hoteles-content-front -p 2222  --verbose &
 az webapp log config cols-as-digprod-ryt-hoteles-content-front --resource-group Digital-Produccion-RyT-Hoteles --docker-container-logging filesystem
 
  docker run -it -p 80:80 -p 8081:2222  --env-file test.env test:test1
